07e3e7a842a10e741c5ea0abe460f0b6037481d7
Enoch Kattumuri <enochkattumuri@gmail.com>
2024-01-22 23:04:27 +0530

    fragment and html authoring

---
+++ Section 04 - JSX/07 - Fragment and HTML Authoring/app.js
@@ -0,0 +1,46 @@
+const rootNode = document.getElementById('app');
+const root = ReactDOM.createRoot(rootNode);
+
+let counterName = "one";
+
+root.render(React.createElement(App));
+
+function App() {
+  const counterOne = <Counter name={counterName} />;
+  const counterTwo = <Counter2 name={counterName} />;
+
+  return (
+    <>
+      <h1>Counters</h1>
+      <section>
+        {counterName === 'one' ? counterOne : counterTwo}
+      </section>
+    </>
+  );
+}
+
+function Counter({ name }) {
+  return (
+    <article>
+      <h2>Counter {name}</h2>
+      <p>You clicked 1 times</p>
+      <button>Click me</button>
+    </article>
+  );
+}
+
+function Counter2({ name }) {
+  return (
+    <article>
+      <h2>Counter {name}</h2>
+      <p>Times clicked: 1</p>
+      <button>Click me</button>
+    </article>
+  );
+}
+
+function rerender() {
+  console.log("Rerendering...");
+  counterName = "two";
+  root.render(React.createElement(App));
+}
